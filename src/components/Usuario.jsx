b43fdd98c60cb8d4b4ee222c81e218a9c4326810
LsB.Dev <prof.luifel@gmail.com>
2023-02-12 00:26:14 -0300

    Trabalhando string vazia

--- src/components/Usuario.jsx
+++ src/components/Usuario.jsx
@@ -4,16 +4,23 @@ export default function Usuario() {
   const [nomeUsuario, setNomeUsuario] = useState("user")
   const [fotoUsuario, setFotoUsuario] = useState("https://www2.sarah.br/Auth/Content/images/user.png")
 
-
   function changeUser() {
     let nome = prompt ('Digite o seu nome')
-    setNomeUsuario(nome)
+    if(nome !== "") {
+      setNomeUsuario(nome)
+    } else {
+      setNomeUsuario(nomeUsuario)
+    }    
   }
+
   function changeFoto() {
     let foto = prompt ('link da sua foto')
-    setFotoUsuario(foto)
-  }  
- 
+    if(foto !== "") {
+      setFotoUsuario(foto)
+    } else {
+      setFotoUsuario(fotoUsuario)
+    }    
+  }   
 
   return (
     <div class="usuario">
