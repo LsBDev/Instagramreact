c9ae1189b8ac7b08a90ac0874b8b3699b7be85ba
LsB.Dev <prof.luifel@gmail.com>
2023-02-12 00:11:44 -0300

    Mudando Nome de usuário e Foto de perfil

--- src/components/Usuario.jsx
+++ src/components/Usuario.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
+
 export default function Usuario() {
+  const [nomeUsuario, setNomeUsuario] = useState("user")
+  const [fotoUsuario, setFotoUsuario] = useState("https://www2.sarah.br/Auth/Content/images/user.png")
+
+
+  function changeUser() {
+    let nome = prompt ('Digite o seu nome')
+    setNomeUsuario(nome)
+  }
+  function changeFoto() {
+    let foto = prompt ('link da sua foto')
+    setFotoUsuario(foto)
+  }  
+ 
+
   return (
     <div class="usuario">
-          <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
-          <div class="texto">
-            <span>
-              <strong>catanacomics</strong>
-              <ion-icon name="pencil"></ion-icon>
-            </span>
-          </div>
-        </div>
+      <img onClick={changeFoto} src={fotoUsuario} alt="imagem de perfil"/>
+      <div class="texto">
+        <span>
+          <strong>{nomeUsuario}</strong>
+          <ion-icon onClick={changeUser} name="pencil"></ion-icon>
+        </span>
+      </div>
+    </div>
   )
 }
